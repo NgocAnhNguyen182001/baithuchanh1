80aec125182ff005358459422286632280d560f7
Ngoc Anh <ngocanhsaker@gmail.com>
2021-09-19 23:38:15 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="\foodandorder\font\themify-icons\themify-icons.css">
+    
+</head>
+<body>
+    <!--navbar section start -->
+    <section class="navbar">
+        <div class="container">
+      <div class="logo">
+          <img src="\foodandorder\img\icon-shop.png" alt="restaurent logo" class="img-responsive">
+      </div>
+      <div class="menu text-right">
+          <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Foods</a></li>
+              <li><a href="">Contact</a></li>
+          </ul>
+      </div>
+
+      <div class="clearfix"></div>
+      </div>
+    </section>
+    <!--navbar section ends here -->
+
+    <!-- food search section start here -->
+    <section class="food-search text-center">
+    <div class="container">
+      
+    <form action="">
+        <input type="search" name="search" placeholder ="search for food">
+        <input type="submit" name="submit" value="search" class="btn btn-primary">
+    </form>
+
+      </div>
+    </section>
+    
+    <!-- food search section ends here -->
+
+    <!-- categories section start here -->
+    <section class="categories">
+    <div class="container">
+      
+    <h2 class="text-center">Explore Foods</h2>
+    <a href="#">
+      <div class="box-3 float-container ">
+          <img src="img/pizza.jpg" alt="pizza" class="img-responsive img-curve">
+
+          <h3 class="float-text text-white">Pizza</h3>
+      </div>
+      </a>
+       
+      <a href="#">
+      <div class="box-3 float-container">
+      <img src="img/burger.jpg" alt="burger" class="img-responsive img-curve">
+
+<h3 class="float-text text-white">Burger</h3>
+      </div>
+</a>
+<a href="#">
+      <div class="box-3 float-container">
+      <img src="img/momo.jpg" alt="momo" class="img-responsive img-curve">
+
+<h3 class="float-text text-white">Momo</h3>
+      </div>
+</a>
+      <div class="clearfix"></div>
+      </div>
+    <!-- categories section ends here -->
+
+    <!-- foodmenu section start here -->
+    <section class="food-menu">
+    <div class="container">
+      <h2 class="text-center">Foods Menu</h2>
+
+      <div class="food-menu-box">
+          <div class="food-menu-img">
+              <img src="img/menu-pizza.jpg" alt="pizza chicken" class="img-responsive img-curve">
+          </div>
+          <div class="food-menu-desc">
+              <h4 >Food titile</h4>
+              <p class="food-price">$2.3</p>
+              <p class="food-detail">
+                  Made with Italian sauce, chicken and organice vegettable.
+              </p>
+              <br>
+              <a href="" class="btn btn-primary">Order Now</a>
+          </div>
+      </div>
+
+      <div class="food-menu-box">
+      <div class="food-menu-img">
+              <img src="img/menu-burger.jpg" alt="pizza chicken" class="img-responsive img-curve">
+          </div>
+          <div class="food-menu-desc">
+              <h4 >Smoky Burger</h4>
+              <p class="food-price">$2.3</p>
+              <p class="food-detail">
+                  Made with Italian sauce, chicken and organice vegettable.
+              </p>
+              <br>
+              <a href="" class="btn btn-primary">Order Now</a>
+          </div>
+      </div>
+
+      <div class="food-menu-box">
+      <div class="food-menu-img">
+              <img src="img/menu-burger.jpg" alt="pizza chicken" class="img-responsive img-curve">
+          </div>
+          <div class="food-menu-desc">
+              <h4 >Nice Burger</h4>
+              <p class="food-price">$2.3</p>
+              <p class="food-detail">
+                  Made with Italian sauce, chicken and organice vegettable.
+              </p>
+              <br>
+              <a href="" class="btn btn-primary">Order Now</a>
+          </div>
+      </div>
+
+      <div class="food-menu-box">
+      <div class="food-menu-img">
+              <img src="img/menu-pizza.jpg" alt="pizza chicken" class="img-responsive img-curve">
+          </div>
+          <div class="food-menu-desc">
+              <h4 >Food titile</h4>
+              <p class="food-price">$2.3</p>
+              <p class="food-detail">
+                  Made with Italian sauce, chicken and organice vegettable.
+              </p>
+              <br>
+              <a href="" class="btn btn-primary">Order Now</a>
+          </div>
+      </div>
+
+      <div class="food-menu-box">
+      <div class="food-menu-img">
+              <img src="img/menu-pizza.jpg" alt="pizza chicken" class="img-responsive img-curve">
+          </div>
+          <div class="food-menu-desc">
+              <h4 >Food titile</h4>
+              <p class="food-price">$2.3</p>
+              <p class="food-detail">
+                  Made with Italian sauce, chicken and organice vegettable.
+              </p>
+              <br>
+              <a href="" class="btn btn-primary">Order Now</a>
+          </div>
+      </div>
+
+      <div class="food-menu-box">
+      <div class="food-menu-img">
+              <img src="img/menu-momo.jpg" alt="pizza chicken" class="img-responsive img-curve">
+          </div>
+          <div class="food-menu-desc">
+              <h4 >Chicken Steam Momo</h4>
+              <p class="food-price">$2.3</p>
+              <p class="food-detail">
+                  Made with Italian sauce, chicken and organice vegettable.
+              </p>
+              <br>
+              <a href="" class="btn btn-primary">Order Now</a>
+          </div>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+</section>
+    <!-- foodmenu section ends here -->
+
+<!-- social section start here -->
+<section class="social">
+<div class="container text-center">
+      <ul>
+          <li><a href=""><i class="ti-facebook"></i></a></li>
+          <li><a href=""><i class="ti-instagram"></i></a></li>
+          <li><a href=""><i class="ti-twitter-alt"></i></a></li>
+      </ul>
+      </div>
+</section>
+<!-- social section ends here -->
+
+<!-- footer section start here -->
+<section class="footer text-center">
+<div class="container">
+      <p>
+          All right reserved. Design By <a href="">Ngoc Anh Nguyen</a>
+      </p>
+      </div>
+<!-- footer section ends here -->
+</section>
+</body>
+</html>
+
+<!-- css for mobile size or smaller screen -->
+
